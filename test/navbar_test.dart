c9228923b4d09291c6462efae449c516053158cd
Willie Pai <PaisWillie@gmail.com>
2022-10-10 09:59:54 -0400

    Added navbar user test

--- test/navbar_test.dart
+++ test/navbar_test.dart
@@ -1,10 +1,11 @@
 import 'package:cosy_pos/widgets/navbar/nav_item.dart';
+import 'package:cosy_pos/widgets/navbar/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cosy_pos/main.dart';
 
 void main() {
-  testWidgets('Nav menu items test', (WidgetTester tester) async {
+  testWidgets('Menu items test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.widgetWithText(NavItem, 'Reservation'), findsOneWidget);
@@ -12,5 +13,13 @@ void main() {
     expect(find.widgetWithText(NavItem, 'Menu'), findsOneWidget);
     expect(find.widgetWithText(NavItem, 'Delivery'), findsOneWidget);
     expect(find.widgetWithText(NavItem, 'Accounting'), findsOneWidget);
+  });
+
+  testWidgets('User test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.widgetWithText(User, 'Leslie K.'), findsOneWidget);
+    expect(find.widgetWithText(User, 'Cameron W.'), findsOneWidget);
+    expect(find.widgetWithText(User, 'Jacob J.'), findsOneWidget);
   });
 }
